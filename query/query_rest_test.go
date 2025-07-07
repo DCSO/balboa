eefed62f49c9d0af75976ba60127420adab040fc
Sascha Steinbiss <satta@debian.org>
2025-07-07 22:33:52 +0200

    extend REST API
    This adds pagination and filtering for rrtype and sensorid
    to the REST API.

--- query/query_rest_test.go
+++ query/query_rest_test.go
@@ -1,12 +1,12 @@
 // balboa
-// Copyright (c) 2020, DCSO GmbH
+// Copyright (c) 2020, 2025, DCSO GmbH
 
 package query
 
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net"
 	"net/http"
 	"strings"
@@ -17,8 +17,38 @@ import (
 	"github.com/DCSO/balboa/observation"
 )
 
-func makeRESTQuery(t *testing.T, keyword, addr string) ([]byte, error) {
-	resp, err := http.Get("http://" + addr + fmt.Sprintf("/pdns/query/%s", keyword))
+func makeRESTQuery(t *testing.T, keyword, addr string, limit, offset *uint64, sensorids []string, rrtype *string) ([]byte, error) {
+	url := "http://" + addr + fmt.Sprintf("/pdns/query/%s", keyword)
+	if limit != nil {
+		url += fmt.Sprintf("?limit=%d", *limit)
+	}
+	if offset != nil {
+		if !strings.Contains(url, "?") {
+			url += "?"
+		} else {
+			url += "&"
+		}
+		url += fmt.Sprintf("next=%d", *offset)
+	}
+	if rrtype != nil {
+		if !strings.Contains(url, "?") {
+			url += "?"
+		} else {
+			url += "&"
+		}
+		url += fmt.Sprintf("rrtype=%s", *rrtype)
+	}
+	if len(sensorids) > 0 {
+		if !strings.Contains(url, "?") {
+			url += "?"
+		} else {
+			url += "&"
+		}
+		url += "sensorids=" + strings.Join(sensorids, ",")
+	}
+	t.Logf("making REST query to %s", url)
+
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -26,7 +56,7 @@ func makeRESTQuery(t *testing.T, keyword, addr string) ([]byte, error) {
 		return nil, fmt.Errorf("status %d", resp.StatusCode)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
@@ -53,7 +83,7 @@ var rq RESTFrontend
 
 func testRESTQueryOne(t *testing.T) {
 	// run queries: foo
-	body, err := makeRESTQuery(t, "foo", rq.GetAddr())
+	body, err := makeRESTQuery(t, "foo", rq.GetAddr(), nil, nil, []string{}, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -68,7 +98,7 @@ func testRESTQueryOne(t *testing.T) {
 
 func testRESTQueryAnother(t *testing.T) {
 	// run queries: foo
-	body, err := makeRESTQuery(t, "bar", rq.GetAddr())
+	body, err := makeRESTQuery(t, "bar", rq.GetAddr(), nil, nil, []string{}, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -83,7 +113,7 @@ func testRESTQueryAnother(t *testing.T) {
 
 func testRESTQueryTwo(t *testing.T) {
 	// run queries: bar
-	body, err := makeRESTQuery(t, "1.2.3.4", rq.GetAddr())
+	body, err := makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), nil, nil, []string{}, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -99,9 +129,110 @@ func testRESTQueryTwo(t *testing.T) {
 	}
 }
 
+func testRESTQuerySensorID(t *testing.T) {
+	body, err := makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), nil, nil, []string{"zbc"}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res := parseRESTResult(t, body)
+	if len(res) != 1 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+
+	}
+	if res[0].RRName != "baz" {
+		t.Fatalf("unexpected result rrname: %s", res[0].RRName)
+	}
+}
+
+func testRESTQueryRRType(t *testing.T) {
+	rrtype := "MX"
+	body, err := makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), nil, nil, []string{}, &rrtype)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res := parseRESTResult(t, body)
+	if len(res) != 1 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+
+	}
+	if res[0].RRName != "baz" {
+		t.Fatalf("unexpected result rrname: %s", res[0].RRName)
+	}
+}
+
+func testRESTQueryPaginate(t *testing.T) {
+	// regular query
+	offset := uint64(0)
+	limit := uint64(2)
+	body, err := makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), &limit, &offset, []string{}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res := parseRESTResult(t, body)
+	if len(res) != 2 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+	}
+	if res[0].RRName != "foo" {
+		t.Fatalf("unexpected result rrname: %s", res[0].RRName)
+	}
+	if res[1].RRName != "baz" {
+		t.Fatalf("unexpected result rrname: %s", res[1].RRName)
+	}
+	// empty result
+	offset = uint64(0)
+	limit = uint64(0)
+	body, err = makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), &limit, &offset, []string{}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res = parseRESTResult(t, body)
+	if len(res) != 0 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+	}
+	// out of bounds
+	offset = uint64(4)
+	limit = uint64(5)
+	body, err = makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), &limit, &offset, []string{}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res = parseRESTResult(t, body)
+	if len(res) != 0 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+	}
+	// limit 1
+	offset = uint64(0)
+	limit = uint64(1)
+	body, err = makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), &limit, &offset, []string{}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res = parseRESTResult(t, body)
+	if len(res) != 1 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+	}
+	if res[0].RRName != "foo" {
+		t.Fatalf("unexpected result rrname: %s", res[0].RRName)
+	}
+	// offset 1
+	offset = uint64(1)
+	limit = uint64(4)
+	body, err = makeRESTQuery(t, "1.2.3.4", rq.GetAddr(), &limit, &offset, []string{}, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res = parseRESTResult(t, body)
+	if len(res) != 1 {
+		t.Fatalf("unexpected length of result: %d", len(res))
+	}
+	if res[0].RRName != "baz" {
+		t.Fatalf("unexpected result rrname: %s", res[0].RRName)
+	}
+}
+
 func testRESTQueryEmpty(t *testing.T) {
 	// run queries: valid query path but nonexisting keyword
-	_, err := makeRESTQuery(t, "nonexist", rq.GetAddr())
+	_, err := makeRESTQuery(t, "nonexist", rq.GetAddr(), nil, nil, []string{}, nil)
 	if err == nil {
 		t.Fatal("missing error")
 	}
@@ -147,9 +278,9 @@ func TestQueryREST(t *testing.T) {
 	inChan <- observation.InputObservation{
 		Rrname:         "baz",
 		Rdata:          "1.2.3.4",
-		Rrtype:         "A",
+		Rrtype:         "MX",
 		Count:          1,
-		SensorID:       "abc",
+		SensorID:       "zbc",
 		TimestampStart: time.Now(),
 		TimestampEnd:   time.Now().Add(1 * time.Second),
 	}
@@ -160,7 +291,7 @@ func TestQueryREST(t *testing.T) {
 
 	// create test frontend on random free port
 	rq = RESTFrontend{}
-	listener, err := net.Listen("tcp", fmt.Sprintf(":0"))
+	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -170,5 +301,8 @@ func TestQueryREST(t *testing.T) {
 	t.Run("QueryTwo", testRESTQueryTwo)
 	t.Run("QueryEmpty", testRESTQueryEmpty)
 	t.Run("QueryFail", testRESTQueryFail)
+	t.Run("QueryPaginate", testRESTQueryPaginate)
+	t.Run("QuerySensorID", testRESTQuerySensorID)
+	t.Run("QueryRRType", testRESTQueryRRType)
 	rq.Stop()
 }
